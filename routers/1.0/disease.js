8aa60b67aeb3d0049f9ee8cffd228a0c63cd97ed
Ihsanul <ihsanboga6415@gmail.com>
2021-07-19 11:25:47 +0700

    add: mongo version controller

--- routers/1.0/disease.js
+++ routers/1.0/disease.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 const { authenticate, bearer, keySecretVerify } = require("../../middlewares");
 
-const { DiseaseController } = require("../../controllers/1.0");
+const {
+  DiseaseController,
+  DiseaseMongoController,
+} = require("../../controllers/1.0");
 
 router.post("/", DiseaseController.list);
+router.post("/mongo", DiseaseMongoController.list);
 router.post("/add", DiseaseController.create);
 router.put("/update", DiseaseController.update);
 router.delete("/delete/:id", DiseaseController.delete);
